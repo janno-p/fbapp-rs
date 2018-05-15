3d455ad60bb2053748f412e4bc60c484b43c5cb1
Janno Põldma <janno.poldma@gmail.com>
2018-05-15 17:18:03 +0300

    Fix windows build.

---
+++ build.rs
@@ -0,0 +1,3 @@
+﻿fn main() {
+    println!(r"cargo:rustc-link-search=C:\Users\Janno\Downloads\postgresql-10.3-3-windows-x64-binaries\pgsql\lib");
+}
--- src/main.rs
+++ src/main.rs
@@ -13,29 +13,49 @@ fn main() {
 
 #[cfg(debug_assertions)]
 fn start_quasar_dev() {
-    use std::process::{Command,Stdio};
+    use std::process::{Command, Stdio};
     use std::path::Path;
 
-    let output = Command::new("yarn")
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .current_dir(Path::new("web"))
-        .arg("install")
-        .output()
-        .unwrap();
-
-    if !output.status.success() {
-        
-        eprintln!("{}", String::from_utf8_lossy(&output.stderr));
-        std::process::exit(1);
+    fn init_command(command: &mut Command) {
+        command.stderr(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .current_dir(Path::new("web"));
     }
 
-    Command::new("yarn")
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .current_dir(Path::new("web"))
-        .arg("quasar")
-        .arg("dev")
-        .spawn()
-        .unwrap() ;
+    #[cfg(target_os = "windows")]
+    fn create_command() -> Command {
+        let mut command = Command::new("cmd");
+        init_command(&mut command);
+        command.arg("/c").arg("yarn.cmd");
+        command
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    fn create_command() -> Command {
+        let mut command = Command::new("yarn");
+        init_command(&mut command);
+        command
+    }
+
+    let output = create_command()
+        .arg("install")
+        .output();
+
+    match output {
+        Ok(ref o) if o.status.success() => {
+            create_command()
+                .arg("quasar")
+                .arg("dev")
+                .spawn()
+                .unwrap();
+        },
+        Ok(o) => {
+            eprintln!("{}", String::from_utf8_lossy(&o.stderr));
+            std::process::exit(1);
+        },
+        Err(e) => {
+            eprintln!("Error occured: {}", e);
+            std::process::exit(1);
+        }
+    }
 }
